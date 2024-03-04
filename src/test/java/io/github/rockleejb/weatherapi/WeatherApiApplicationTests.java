c24ac68e31f8ae76223a7f73d4eabe361ac49dad
Jan Balangue <janbalangue@Jans-MacBook-Air.local>
2024-03-04 12:43:08 -0600

    first commit

---
+++ src/test/java/io/github/rockleejb/weatherapi/WeatherApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.rockleejb.weatherapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
