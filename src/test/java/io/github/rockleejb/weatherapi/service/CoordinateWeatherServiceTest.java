2de50c349e656fbeb9360ae8e662cb841f1d8823
Jan Balangue <janbalangue@Jans-MacBook-Air.local>
2024-03-06 14:12:51 -0600

    add dependency

--- src/test/java/io/github/rockleejb/weatherapi/service/CoordinateWeatherServiceTest.java
+++ src/test/java/io/github/rockleejb/weatherapi/service/CoordinateWeatherServiceTest.java
@@ -3,7 +3,9 @@ package io.github.rockleejb.weatherapi.service;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
+import okhttp3.mockwebserver.RecordedRequest;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -21,7 +23,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class CoordinateWeatherServiceTest {
     private static MockWebServer mockBackEnd;
     private CoordinateWeatherService coordinateWeatherService;
-    private ObjectMapper objectMapper;
+    private final ObjectMapper objectMapper = new ObjectMapper();
     @BeforeAll
     static void setUp() throws IOException {
         mockBackEnd = new MockWebServer();
@@ -34,9 +36,28 @@ class CoordinateWeatherServiceTest {
                 mockBackEnd.getPort());
         coordinateWeatherService = new CoordinateWeatherService(baseUrl, objectMapper);
     }
+
+    @Test
+    void getWeatherByCoordinates() throws IOException, InterruptedException {
+//        InputStream in = Thread.currentThread().getContextClassLoader().getResourceAsStream("WeatherApiResponse.json");
+//        JsonNode jsonNode = objectMapper.readValue(in, JsonNode.class);
+//        mockBackEnd.enqueue(new MockResponse()
+//                .setBody(objectMapper.writeValueAsString(jsonNode))
+//                .addHeader("Content-Type", "application/json"));
+//        RecordedRequest recordedRequest = mockBackEnd.takeRequest();
+//        Map<String, Object> response = coordinateWeatherService.getWeatherByCoordinates("41.8781", "-87.6298");
+//        assertAll(
+//                () -> assertEquals("GET", recordedRequest.getMethod()),
+//                () -> assertNotNull(response.get("coordinates")),
+//                () -> assertNotNull(response.get("weather")),
+//                () -> assertNotNull(response.get("main")),
+//                () -> assertEquals("Chicago", response.get("city")),
+//                () -> assertNull(response.get("id"))
+//        );
+    }
+
     @Test
     void transformResponse() throws IOException {
-        objectMapper = new ObjectMapper();
         InputStream in = Thread.currentThread().getContextClassLoader().getResourceAsStream("OwmResponse.json");
         JsonNode jsonNode = objectMapper.readValue(in, JsonNode.class);
         Map<String, Object> stringObjectMap = objectMapper.convertValue(jsonNode, new TypeReference<>() {});
